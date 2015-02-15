5054c960ffd551dd6a8a79374ab2d11b517adfa3
Pascal Wever <p@wever.co>
2015-02-14 21:46:54 -0800

    added ailes to products; form based image upload; context filter; iphone layout;

--- GroceryList.js
+++ GroceryList.js
@@ -1,11 +1,27 @@
 // Grocery products
 Products = new Mongo.Collection("products");
+Ailes = new Mongo.Collection("ailes");
+// Stores = new Mongo.Collection("stores");
 
 
 
 if (Meteor.isClient) {
 
+
+
   Template.body.helpers({
+    selected_product: function() {
+      var product = Products.findOne(Session.get("selected_product"));
+      return product;
+    }
+  });
+
+
+
+
+
+
+  Template.shopping_list.helpers({
     products: function() {
       return Products.find({}, {sort: {name: 1}});
     }
@@ -14,7 +30,7 @@ if (Meteor.isClient) {
 
 
 
-  
+
 
   Template.nav.events({
 
@@ -36,7 +52,7 @@ if (Meteor.isClient) {
 
     "keyup .new_product": function(evt) {
       // get the needle
-      console.log(evt.target.value);
+      // console.log(evt.target.value);
       var needle = evt.target.value;
 
       // filter Product
@@ -46,7 +62,7 @@ if (Meteor.isClient) {
 
     "search .new_product": function(evt) {
       // get the needle
-      console.log(evt.target.value);
+      // console.log(evt.target.value);
       var needle = evt.target.value;
 
       // filter Product
@@ -58,7 +74,7 @@ if (Meteor.isClient) {
       $("nav ul li").removeClass("active");
       $(evt.target).addClass("active");
       // shoppinglist
-      console.log($(evt.target).data("filter"));
+      // console.log($(evt.target).data("filter"));
       $("#shoppinglist").removeClass().addClass($(evt.target).data("filter"));
     }
 
@@ -68,17 +84,21 @@ if (Meteor.isClient) {
 
 
 
-
-
-
-
-
+  
 
 
   Template.product.events({
 
-    "change li label input[type~=checkbox]": function(event) {
-      Products.update(this._id, {$set: {isNeeded: !this.isNeeded}});
+    "click li": function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      
+      if (event.altKey) {
+        Session.set("selected_product", this._id);
+        // console.log("show selected product details");
+      } else {
+        Products.update(this._id, {$set: {isNeeded: !this.isNeeded}});
+      }
     },
 
     "dragenter li": function(event) {
@@ -121,6 +141,66 @@ if (Meteor.isClient) {
   });
 
 
+
+  Template.detail.helpers({
+    selected_product: function() {
+      var product = Products.findOne(Session.get("selected_product"));
+      return product;
+    },
+
+    ailes: function() {
+      // var product = Session.get("selected_product");
+
+      var ailes = Ailes.find({}, {sort: {name: 1}});
+
+      return ailes;
+    },
+
+    inaile: function() {
+      var product = Products.findOne(Session.get("selected_product"));
+      return (product.aile && product.aile==this.name) ? "checked" : '';
+    }
+  });
+
+
+  Template.detail.events({
+    "click #close_button": function(evt) {
+      Session.set("selected_product", null);
+    },
+
+    "click #delete_button": function(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+
+      Products.remove(Session.get("selected_product"));
+      Session.set("selected_product", null);
+    },
+
+    
+    "submit form": function(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+
+      var product_id = Session.get("selected_product");
+      var new_name = evt.target.name.value;
+      var new_aile = $("#detail form input[name=aile]:checked").val();
+
+      Products.update(product_id, {$set: {name: new_name, aile: new_aile}});
+
+      Session.set("selected_product", null);
+
+      // upload file
+      var uploads = $(evt.currentTarget).find("input[type=file]")[0].files;
+      for (var f in uploads) {
+        if (uploads[f] instanceof File) {
+          var ext = uploads[f].name.substr(uploads[f].name.lastIndexOf('.'));
+          Meteor.saveFile(uploads[f], product_id + ext);
+          Products.update(product_id, {$set: {"image": product_id  + ext }});
+        }
+      }
+    }
+    
+  });
 
 
 
